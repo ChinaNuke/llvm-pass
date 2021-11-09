91d8bb2fd58fc3e14ac751d8a2405c321c5cc41d
ChinaNuke <chinanuke@nuke666.cn>
2021-11-09 20:29:08 +0800

    实现直接函数调用和PHI结点处理，通过测试用例00到03

--- LLVMAssignment.cpp
+++ LLVMAssignment.cpp
@@ -23,6 +23,7 @@
 #include <llvm/Transforms/Utils.h>
 
 #include <llvm/IR/Function.h>
+#include <llvm/IR/Instructions.h>
 #include <llvm/Pass.h>
 #include <llvm/Support/raw_ostream.h>
 
@@ -66,12 +67,99 @@ char EnableFunctionOptPass::ID = 0;
 struct FuncPtrPass : public ModulePass {
   static char ID; // Pass identification, replacement for typeid
   FuncPtrPass() : ModulePass(ID) {}
+  std::map<unsigned int, std::vector<std::string>> results;
+  std::vector<std::string> tempFuncNames;
 
+private:
+  void addFuncName(const std::string funcName) {
+    tempFuncNames.push_back(funcName);
+  }
+
+  void saveResultAndClearTemp(const unsigned int lineno) {
+    auto result = results.find(lineno);
+    if (result == results.end()) {
+      // 看起来 pair 是把作为参数的 vector 又复制了一份，
+      // 所以后面将 vector 清空不会影响输出
+      results.insert(std::pair<unsigned int, std::vector<std::string>>(
+          lineno, tempFuncNames));
+    } else {
+      LOG_DEBUG("I think this will never appear!");
+      // auto &funcNames = result.second;
+      // funcNames.insert(funcNames.end(), tempFuncNames.begin(),
+      // tempFuncNames.end());
+    }
+    tempFuncNames.clear();
+  }
+
+  void printResults() const {
+    for (const auto &out : results) {
+      errs() << out.first << ": ";
+      const auto &funcNames = out.second;
+      for (auto iter = funcNames.begin(); iter != funcNames.end(); iter++) {
+        if (iter != funcNames.begin()) {
+          errs() << ",";
+        }
+        errs() << *iter;
+      }
+      errs() << "\n";
+    }
+  }
+
+  void handlePHINode(const PHINode *phiNode) {
+    for (const Value *value : phiNode->incoming_values()) {
+      LOG_DEBUG("Incoming value for PHINode: " << *value);
+      if (const Function *func = dyn_cast<Function>(value)) {
+        // LOG_DEBUG("Function in PHINode values: " << func->getName().data());
+        addFuncName(func->getName().data());
+      } else if (const PHINode *innerPHINode = dyn_cast<PHINode>(value)) {
+        handlePHINode(innerPHINode);
+      } else {
+        LOG_DEBUG("Unhandled PHINode incoming value: " << *value);
+      }
+    }
+  }
+
+  void handleCallInst(const CallInst *callInst) {
+    // getCalledFunction returns the function called,
+    // or null if this is an indirect function invocation.
+    if (const Function *func = callInst->getCalledFunction()) {
+      // 直接函数调用（或者已经被优化成直接调用的间接函数调用）
+      std::string funcName = func->getName().data();
+      if (funcName == "llvm.dbg.value") {
+        return;
+      }
+      addFuncName(funcName);
+    } else { // 非直接函数调用
+      LOG_DEBUG("Indirect function invocation: " << *callInst);
+      const Value *value = callInst->getCalledOperand();
+      LOG_DEBUG("Value of indirect function invocation: " << *value);
+      if (const PHINode *phiNode = dyn_cast<PHINode>(value)) {
+        handlePHINode(phiNode);
+      } else {
+        LOG_DEBUG("Unhandled CallOperand Value: " << *value);
+      }
+    }
+    saveResultAndClearTemp(callInst->getDebugLoc().getLine());
+  }
+
+public:
   bool runOnModule(Module &M) override {
     errs() << "Hello: ";
     errs().write_escaped(M.getName()) << '\n';
     M.dump();
     errs() << "------------------------------\n";
+
+    for (const Function &f : M) {
+      for (const BasicBlock &bb : f) {
+        for (const Instruction &i : bb) {
+          if (const CallInst *callInst = dyn_cast<CallInst>(&i)) {
+            handleCallInst(callInst);
+          }
+        }
+      }
+    }
+    printResults();
+
     return false;
   }
 };
